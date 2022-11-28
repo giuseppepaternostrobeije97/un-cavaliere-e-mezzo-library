4ccd790e656bda5c2f71f56a8de14b282cee74fc
pater97 <giuseppepater997@gmail.com>
2022-11-28 17:36:36 +0100

    mokup lobby completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Login from "./forDist/screens/Login";
 import Register from "./forDist/screens/Register";
+import Lobby from "./forDist/screens/Lobby";
 
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      <Login />
+      {/* <Login /> */}
+      <Lobby/>
     </div>
   );
 }
---
+++ src/forDist/screens/Lobby.js
@@ -0,0 +1,154 @@
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
+import React from "react";
+import BG from "../assets/bg.png";
+import CASTLE from "../assets/castle.png";
+
+// colori
+const brandColor = "#232726";
+const secondaryColor = "#77523B";
+// mokup
+const userName = "pincopallo";
+const score = 13;
+
+const Lobby = () => {
+  return (
+    <View style={styles.container}>
+      {/* bgImage */}
+      <ImageBackground
+        source={BG}
+        resizeMode={"cover"}
+        style={styles.bgImage}
+      />
+      {/* titleSection */}
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>ARENE</Text>
+      </View>
+      {/* userSection */}
+      <View className="userSection" style={styles.userSection}>
+        <View style={styles.userName}>
+          <Text style={styles.normalText}>{userName}</Text>
+        </View>
+        <View style={styles.score}>
+          <Text style={styles.normalText}>SCORE</Text>
+          <View style={styles.uiLine}></View>
+          <Text style={styles.normalText}>{score}</Text>
+        </View>
+      </View>
+      {/* arenaListSection */}
+      <View style={styles.arenasListConainter}>
+        <View style={styles.arenasList}>
+          <Text style={styles.sectionTitle}>
+            LISTA ARENE
+          </Text>
+        </View>
+      </View>
+      {/* create new arena */}
+      <View style={styles.arenasListConainter}>
+        <View style={styles.arenasList}>
+          <Text style={styles.sectionTitle}>
+            CREA ARENA
+          </Text>
+        </View>
+      </View>
+      {/* image castle */}
+      <Image source={CASTLE} resizeMode={"cover"} style={styles.imageCastle} />
+    </View>
+  );
+};
+
+export default Lobby;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+  },
+  bgImage: {
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    zIndex: 1,
+  },
+  titleContainer: {
+    backgroundColor: secondaryColor,
+    width: "100%",
+    height: "10%",
+    zIndex: 2,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  imageCastle: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    zIndex: 2,
+    width: "100%",
+    height: "40%",
+  },
+  userSection: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: "20%",
+    zIndex: 3,
+    padding: 20,
+  },
+  userName: {
+    width: "75%",
+    height: "100%",
+    backgroundColor: secondaryColor,
+    borderRadius: 5,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  score: {
+    width: "20%",
+    height: "100%",
+    backgroundColor: secondaryColor,
+    borderRadius: 5,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  arenasListConainter: {
+    height: "20%",
+    zIndex: 3,
+    padding: 20,
+    width: "100%",
+  },
+  arenasList: {
+    backgroundColor: secondaryColor,
+    height: "100%",
+    width: "100%",
+    borderRadius: 5,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  normalText: {
+    color: "#fff",
+    fontSize: 20,
+    textTransform: "uppercase",
+  },
+  uiLine: {
+    backgroundColor: "#fff",
+    height: 2,
+    width: "100%",
+  },
+  sectionTitle:{
+    color:'#fff',
+    fontSize:30,
+    fontWeight:'bold'
+  }
+});
